4fa7509cb790cff78923474a1e69c9e25632ff28
gmalone03 <122484941+gmalone03@users.noreply.github.com>
2023-03-01 12:47:54 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,35 +1,48 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-# Clone the repository of the student submission to a well-known directory name
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
 
-# Check that the student code has the correct file submitted. If they didn’t, detect and give helpful feedback about it.
-if [ ! -f student-submission/ListExamples.java ]; then
-  echo "Error: 'ListExamples.java' file not found in submission. Please check the file name and try again."
-  exit 1
-fi
-
-# Copy the test file into the same directory as the student code
-cp student-submission/ListExamples.java ./
-
-# Compile your tests and the student’s code from the appropriate directory with the appropriate classpath commands. If the compilation fails, detect and give helpful feedback about it.
-# javac -cp $CPATH *.java
-
-javac -cp lib/*:.TestListExamples.java
-
-# Compile ListExamples.java
-javac ListExamples.java
-
-# Check if the compilation was successful
-if [ $? -eq 0 ]; then
-  echo "Compilation successful!"
+# Recursively search for ListExamples.java
+if [[ $(find . -type f -name "ListExamples.java") ]]; then
+    echo "File with correct name was submitted (ListExamples.java)"
+    # Compile ListExamples.java
+    javac $(find . -type f -name "ListExamples.java")
+    # Check if the compilation was successful
+    if [ $? -eq 0 ]; then
+      echo "Compilation successful!"
+    else
+      echo "Compilation failed. Here are the errors:"
+      javac $(find . -type f -name "ListExamples.java") 2>&1 | grep -v "^Note: "
+    fi
 else
-  echo "Compilation failed. Here are the errors:"
-  javac ListExamples.java 2>&1 | grep -v "^Note: "
+    echo "File with incorrect name was submitted. Rename to ListExamples.java"
+    exit
 fi
 
-# Run the tests and report the grade based on the JUnit output.
-# java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
-java -cp lib/*:. TestListExamples
+
+cd ../
+
+cp student-submission/ListExamples.java ./
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+    echo "There was an error compling the file"
+    cat error-trace.txt
+    exit
+else 
+    echo "The java code was compiled successfully"
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test-output.txt
+
+echo "--------"
+last_line=$(tail -n 2 test-output.txt)
+echo $last_line
+echo "--------"
+echo "Execution ended"
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
