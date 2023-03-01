ab1f1382ab3fae3ce7f2c4f21a4844066b359338
gmalone03 <122484941+gmalone03@users.noreply.github.com>
2023-03-01 11:31:46 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+# Clone the repository of the student submission to a well-known directory name
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
