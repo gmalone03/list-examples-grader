fd992b468ccb84e9916475df87c4b60f8d7a0a4c
gmalone03 <122484941+gmalone03@users.noreply.github.com>
2023-03-01 11:24:25 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-# Changes working directory of script to directory where the script is located
-cd "$(dirname "$0")"
-
-# Clone the repository of the student submission to a well-known directory name (provided in starter code)
 rm -rf student-submission
-git clone "$1" student-submission
+git clone $1 student-submission
+echo 'Finished cloning'
+
+cp student-submission/ListExamples.java ./
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
 
 # Check that the student code has the correct file submitted. If they didn’t, detect and give helpful feedback about it.
 if [ ! -f student-submission/ListExamples.java ]; then
@@ -18,16 +19,7 @@ fi
 cp TestListExamples.java student-submission/
 
 # Compile your tests and the student’s code from the appropriate directory with the appropriate classpath commands. If the compilation fails, detect and give helpful feedback about it.
-cd student-submission
-if ! javac -cp .:/usr/share/java/junit4.jar ListExamples.java TestListExamples.java; then
-  echo "Error: Compilation failed. Please check your code and try again."
-  exit 1
-fi
+
 
 # Run the tests and report the grade based on the JUnit output.
-java -cp .:/usr/share/java/junit4.jar org.junit.runner.JUnitCore TestListExamples > test_output.txt 2>&1
-if grep -q "FAILURES!!!" test_output.txt; then
-  echo "Fail"
-else
-  echo "Pass"
-fi
+
